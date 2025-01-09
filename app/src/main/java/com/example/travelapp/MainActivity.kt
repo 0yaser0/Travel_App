e9869c406fc10f9cd3c7633c428e1dea5f1e053e
Yaser <yaseryjjou@gmail.com>
2025-01-09 13:26:42 +0100

    buttom bar not displaying

--- app/src/main/java/com/example/travelapp/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.example.travelapp.components.TravelAppBar
-import com.example.travelapp.components.TravelAppBottomBar
 import com.example.travelapp.components.MainContent
+import com.example.travelapp.components.TravelAppBottomBar
 import com.example.travelapp.ui.theme.TravelAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,9 +26,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TravelAppScaffold() {
+    val navController = rememberNavController()
+
     Scaffold(
         topBar = { TravelAppBar() },
-        bottomBar = { TravelAppBottomBar() },
+        bottomBar = { TravelAppBottomBar(navController) },
         content = { innerPadding ->
             MainContent(modifier = Modifier.padding(innerPadding).fillMaxSize())
         }
